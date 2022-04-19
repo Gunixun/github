deaf4f70558a84237c8d7b89a566bcef2c118e46
GanievAR <ganievar@bnipi.rosneft.ru>
2022-04-19 17:13:58 +0500

    добавлен базовый фрагмент с моделью + базовая модель

---
+++ app/src/main/java/gunixun/github/ui/BaseFragmentWithModel.kt
@@ -0,0 +1,24 @@
+package gunixun.github.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewbinding.ViewBinding
+import java.lang.reflect.ParameterizedType
+
+abstract class BaseFragmentWithModel<ViewModelType : BaseViewModel, VB : ViewBinding>(private val bindingInflater: (inflater: LayoutInflater) -> VB) :
+    BaseFragment<VB>(bindingInflater) {
+    lateinit var viewModel: ViewModelType
+
+    private fun initViewModel() {
+        val parameterizedType = javaClass.genericSuperclass as? ParameterizedType
+        val vmClass = parameterizedType?.actualTypeArguments?.getOrNull(0) as? Class<ViewModelType>?
+        if (vmClass != null)
+            viewModel = ViewModelProvider(this).get(vmClass)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initViewModel()
+    }
+}
---
+++ app/src/main/java/gunixun/github/ui/BaseViewModel.kt
@@ -0,0 +1,5 @@
+package gunixun.github.ui
+
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel : ViewModel()
