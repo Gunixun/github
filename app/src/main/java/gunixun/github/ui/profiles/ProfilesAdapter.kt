21d31523aae08e3f9a8f2688f586bdea9f469394
GanievAR <ganievar@bnipi.rosneft.ru>
2022-04-20 21:17:05 +0500

    Изменил название файла с разметкой айтема

--- app/src/main/java/gunixun/github/ui/profiles/ProfilesAdapter.kt
+++ app/src/main/java/gunixun/github/ui/profiles/ProfilesAdapter.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import coil.load
 import androidx.recyclerview.widget.RecyclerView
-import gunixun.github.databinding.FragmentRecyclerItemProfileBinding
+import gunixun.github.databinding.FragmentProfileItemBinding
 import gunixun.github.domain.entities.Profile
 import gunixun.github.utils.Change
 import gunixun.github.utils.createCombinePayloads
@@ -38,7 +38,7 @@ class ProfilesAdapter : RecyclerView.Adapter<ProfilesAdapter.ProfileViewHolder>(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileViewHolder {
-        val binding = FragmentRecyclerItemProfileBinding.inflate(
+        val binding = FragmentProfileItemBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
@@ -63,12 +63,12 @@ class ProfilesAdapter : RecyclerView.Adapter<ProfilesAdapter.ProfileViewHolder>(
             val newData = change.newData
 
             if (oldData.name != newData.name){
-                FragmentRecyclerItemProfileBinding.bind(holder.itemView).nameTextView.text =
+                FragmentProfileItemBinding.bind(holder.itemView).nameTextView.text =
                     newData.name
             }
 
             if (oldData.login != newData.login){
-                FragmentRecyclerItemProfileBinding.bind(holder.itemView).loginTextView.text =
+                FragmentProfileItemBinding.bind(holder.itemView).loginTextView.text =
                     newData.login
             }
 
@@ -79,7 +79,7 @@ class ProfilesAdapter : RecyclerView.Adapter<ProfilesAdapter.ProfileViewHolder>(
 
     inner class ProfileViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         fun bind(data: Profile) {
-            FragmentRecyclerItemProfileBinding.bind(itemView).apply {
+            FragmentProfileItemBinding.bind(itemView).apply {
                 avatarImageView.load(data.avatar_url)
                 loginTextView.text = data.login
                 nameTextView.text = data.name
